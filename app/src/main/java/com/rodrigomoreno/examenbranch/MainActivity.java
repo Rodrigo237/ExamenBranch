cd8e67187052e1d6b93b5fe0ea9608fa794dbca4
Rodrigo237 <36214941+Rodrigo237@users.noreply.github.com>
2020-12-09 08:41:20 -0600

    Creacion

---
+++ app/src/main/java/com/rodrigomoreno/examenbranch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rodrigomoreno.examenbranch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
